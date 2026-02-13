3e2cf298c8122d1943442e40f51a5a187237e329
User <akniet28022004@gmail.com>
2026-02-13 10:20:10 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type React from "react"
 import type { Metadata, Viewport } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Inter } from "next/font/google";
 import "./globals.css"
 
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
+const _geist = Inter({ subsets: ["latin"] });
+const _geistMono = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Механик - Путевой лист",
